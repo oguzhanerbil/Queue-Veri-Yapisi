36b14d382debc13e270d8740e0ea2f58b0edd771
Oğuzhan Erbil <82578095+oguzhanerbil@users.noreply.github.com>
2024-03-07 23:41:13 +0300

    .

--- QueueVeriYapilari/QueueVeriYapilari/Queue.cpp
+++ QueueVeriYapilari/QueueVeriYapilari/Queue.cpp
@@ -11,22 +11,55 @@ public:
 	QueueNode(const int& data = 0,QueueNode* next = NULL) : data(data),next(next){}
 };
 class Queue {
-
 	QueueNode* root;
+	QueueNode* tail;
+	int length;
 	public:
-		Queue() : root(NULL){}
+		Queue() : root(NULL),tail(NULL), length(0) {}
 		bool isEmpty()const { return root == NULL; }
 		// Kuyruða kabul etme fonksiyonu
 		void enqueue(const int& value) {
-			if (isEmpty()) 
+			if (isEmpty()) {
 				root = new QueueNode(value);
-			else {
-				QueueNode* tmp = root;
-				while (tmp->next != NULL)
-					tmp = tmp->next;
-				tmp->next = new QueueNode(value);
+				tail = root;
 			}
-			
+			else {
+				tail->next = new QueueNode(value);
+				tail = tail->next;
+			};
+			length++;
+		}
+		void dequeue() {
+			if (isEmpty()) {
+				throw "Error : Queue::dequeue() for queue is empty";
+			}
+			QueueNode* tmp = root;
+			if(root == tail){
+				tail = tmp->next;
+			}
+			root = root->next;
+			delete tmp;
+			length--;
+		}
+		void print()const {
+			QueueNode* tmp = root;
+			while (tmp != NULL) {
+				cout << tmp->data << " ";
+				tmp = tmp->next;
+			}
+			cout << endl;
+		}
+		int size()const {
+			return length;
+			/*
+			QueueNode* tmp = root;
+			int counter = 0;
+			while (tmp != NULL) {
+				counter++;
+				tmp = tmp->next;
+			}
+			return counter;
+			*/
 		}
 
 };
@@ -35,5 +68,17 @@ int main() {
 	cout << q.isEmpty() << endl;
 	q.enqueue(10);
 	cout << q.isEmpty() << endl;
+	q.print();
+	q.enqueue(3);
+	q.print();
+	q.dequeue();
+	cout << q.isEmpty() << endl;
+	q.print();
+	q.dequeue();
+	cout << q.isEmpty() << endl;
+	q.enqueue(50);
+	cout << q.isEmpty() << endl;
+	q.print();
+
 	return 0;
 }
